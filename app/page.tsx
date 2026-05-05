14b60428b5e77aa2c36d14908c09940daee6813a
Jason Kim <jkim0836@gmail.com>
2026-05-05 11:12:33 -0700

    delted it

--- app/page.tsx
+++ app/page.tsx
@@ -116,17 +116,4 @@ export default function Home() {
       )} 
     </main>
   );
-import type { AppProps } from 'next/app';
-import { Analytics } from '@vercel/analytics/next';
- 
-function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Component {...pageProps} />
-      <Analytics />
-    </>
-  );
-}
- 
-export default MyApp;
 }
